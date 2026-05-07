002ae4c436ad1360d76cf23d89d7a3059e914e13
daniel huffman <drhuffman123@gmail.com>
2026-05-06 20:54:21 -0400

    add Library examples

---
+++ spec/library_spec.cr
@@ -0,0 +1,83 @@
+require "spec"
+require "../src/library.cr"
+
+describe Library do
+  context "Task 1" do
+    it "test 1" do
+      Library.first_letter("Mystery of the Crystal Skull").should eq 'M'
+    end
+
+    it "test 2" do
+      Library.first_letter("Cave and the Crystals").should eq 'C'
+    end
+
+    it "test 3" do
+      Library.first_letter("A Crystals story").should eq 'A'
+    end
+  end
+
+  context "Task 2" do
+    it "test 1" do
+      Library.initials("William", "Shakespeare").should eq "W.S"
+    end
+
+    it "test 2" do
+      Library.initials("George", "Orwell").should eq "G.O"
+    end
+
+    it "test 3" do
+      Library.initials("Karin", "Boye").should eq "K.B"
+    end
+  end
+
+  context "Task 3" do
+    it "test 1" do
+      Library.decrypt_character('B').should eq 'A'
+    end
+
+    it "test 2" do
+      Library.decrypt_character('C').should eq 'B'
+    end
+
+    it "test 3" do
+      Library.decrypt_character('A').should eq 'Z'
+    end
+
+    it "test 4" do
+      Library.decrypt_character('Z').should eq 'Y'
+    end
+
+    it "test 5" do
+      Library.decrypt_character('a').should eq 'z'
+    end
+
+    it "test 6" do
+      Library.decrypt_character('z').should eq 'y'
+    end
+  end
+
+  context "Task 4" do
+    it "test 1" do
+      expected = "Hello World!"
+      Library.decrypt_text("Ifmmp Xpsme!").should eq expected
+    end
+
+    it "test 2" do
+      expected = "A town by the area 1000 with the capital."
+      Library.decrypt_text("B upxo cz uif bsfb 1000 xjui uif dbqjubm.").should eq expected
+    end
+
+    it "test 3" do
+      expected = "The quick brown fox jumps over the lazy dog."
+      Library.decrypt_text("Uif rvjdl cspxo gpy kvnqt pwfs uif mbaz eph.").should eq expected
+    end
+
+    it "test 4" do
+      expected = "The quick brown fox jumps $%.!?& over the lazy dog."
+      Library.decrypt_text("Uif rvjdl cspxo gpy kvnqt $%.!?& pwfs uif mbaz eph.").should eq expected
+    end
+  end
+end
+
+
+
---
+++ src/library.cr
@@ -0,0 +1,33 @@
+class Library
+  def self.first_letter(title : String) : Char
+    title[0]
+  end
+
+  def self.initials(first_name : String, last_name : String) : String
+    "#{first_letter(first_name)}.#{first_letter(last_name)}"
+  end
+
+  def self.decrypt_character(character : Char) : Char
+    do_convert = ('A' <= character && character <= 'Z') || ('a' <= character && character <= 'z')
+    if do_convert
+      if character == 'A'
+        'Z'
+      elsif character == 'a'
+        'z'    
+      else
+        character.pred
+      end
+    else
+      character
+    end
+  
+  end
+
+  def self.decrypt_text(text : String) : String
+    result = ""
+    text.each_char do |char|
+      result += decrypt_character(char).to_s
+    end
+    result
+  end
+end
