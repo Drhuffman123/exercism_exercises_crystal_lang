6e7fa6940ef4dd685a48079734b3612fda45607e
daniel huffman <drhuffman123@gmail.com>
2026-05-06 20:07:59 -0400

    chess

--- src/chess.cr
+++ src/chess.cr
@@ -1,11 +1,14 @@
 class Chess
   # the 'RANKS' constant
-  RANKS = Range.new(1,8)
+  RANKS = Range.new(1,8, exclusive: false)
+  
   # the 'FILES' constant
-  FILES = Range.new('A','H')
+  FILES = Range.new('A','H', exclusive: false)
 
   def self.valid_square?(rank, file) : Bool
-    RANKS.includes?(rank) && FILES.includes?(file)
+    rank_valid = RANKS.begin.to_s <= rank.to_s && rank.to_s <= RANKS.end.to_s
+    file_valid = FILES.begin.to_s <= file.to_s && file.to_s <= FILES.end.to_s
+    rank_valid && file_valid
   end
 
   def self.nickname(first_name, last_name) : String
@@ -14,17 +17,13 @@ class Chess
 
   def self.move_message(first_name : String, last_name : String, square : String) # : String
     nn = nickname(first_name,last_name)
-    square
-    square.class == String
-    square.size == 2
-    # s0 = square[0]
-    # s1 = square[1]
-    # sq_valid = valid_square?(s0, s1)
-    # if sq_valid
-    #   "#{nn} moved to #{square}"
-    # else
-    #   "#{nn} attempted to move to #{square}, but that is not a valid square"
-    # end
-    # "TBD"
+    rank = square[1]
+    file = square[0]
+    sq_valid = valid_square?(rank, file)
+    if sq_valid
+      "#{nn} moved to #{square}"
+    else    
+      "#{nn} attempted to move to #{square}, but that is not a valid square"
+    end
   end
-end
+end        
