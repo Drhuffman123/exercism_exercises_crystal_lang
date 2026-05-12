63c15fd03d95212801fdfc3b5cd12221bb4c3bb7
daniel huffman <drhuffman123@gmail.com>
2026-05-12 19:16:07 -0400

    ColossalCoaster

--- spec/colossal_coaster_spec.cr
+++ spec/colossal_coaster_spec.cr
@@ -2,15 +2,88 @@ require "spec"
 require "../src/colossal_coaster.cr"
 
 describe ColossalCoaster do
-  context "Test 1" do
-    it "test 1" do
-      # coasters = ColossalCoaster.add_to_queue(express_queue, normal_queue, ticket_type, person_name)
-      # coasters.should eq ["Python"]
-      #
+  context "TASK 1" do
+    it "Test 1" do
       express_queue = ["Tony", "Bruce"]
       normal_queue = ["RobotGuy", "WW"]
       ColossalCoaster.add_to_queue(express_queue, normal_queue, 1, "RichieRich").should eq ["Tony", "Bruce", "RichieRich"]
-      # => ["Tony", "Bruce", "RichieRich"]
+    end
+
+    it "Test 2" do
+      expected = ["Tony", "Bruce", "RichieRich"]
+      ColossalCoaster.add_to_queue(["Tony", "Bruce"], ["RobotGuy", "WW"], 1, "RichieRich").should eq expected
+    end
+  end
+
+  context "TASK 2" do
+    it "Test 3" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      ColossalCoaster.find_my_friend(queue, "Natasha").should eq 0
+    end
+
+    it "Test 4" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      ColossalCoaster.find_my_friend(queue, "Tchalla").should eq 2
+    end
+
+    it "Test 5" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      ColossalCoaster.find_my_friend(queue, "Rocket").should eq 4
+    end
+  end
+
+  context "TASK 3" do
+    it "Test 6" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      expected = ["Bucky", "Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      ColossalCoaster.find_my_friend(queue, "Natasha").should eq 0
+      ColossalCoaster.add_with_friends(queue, "Natasha", "Bucky").should eq expected
+    end
+
+    it "Test 7" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      expected = ["Natasha", "Steve", "Tchalla", "Bucky", "Wanda", "Rocket"]
+      ColossalCoaster.add_with_friends(queue, "Wanda", "Bucky").should eq expected
+    end
+
+    it "Test 8" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      expected = ["Natasha", "Steve", "Tchalla", "Wanda", "Bucky", "Rocket"]
+      ColossalCoaster.add_with_friends(queue, "Rocket", "Bucky").should eq expected
+    end
+  end
+
+  context "TASK 4" do
+    it "Test 9" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      expected = ["Natasha", "Tchalla", "Wanda", "Rocket"]
+      ColossalCoaster.remove_from_queue(queue, "Steve").should eq expected
+    end
+
+    it "Test 10" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      expected = ["Steve", "Tchalla", "Wanda", "Rocket"]
+      ColossalCoaster.remove_from_queue(queue, "Natasha").should eq expected
+    end
+
+    it "Test 11" do
+      queue = ["Natasha", "Steve", "Tchalla", "Wanda", "Rocket"]
+      expected = ["Natasha", "Steve", "Tchalla", "Wanda"]
+      ColossalCoaster.remove_from_queue(queue, "Rocket").should eq expected
+    end
+  end
+
+  context "TASK 5" do
+    it "Test 12" do
+      queue = ["Steve", "Ultron", "Natasha", "Rocket"]
+      expected = ["Natasha", "Rocket", "Steve", "Ultron"]
+      ColossalCoaster.sorted_queue(queue).should eq expected
+    end
+
+    it "Test 13" do
+      queue = ["Steve", "Ultron", "Natasha", "Rocket", "Tchalla"]
+      expected = ["Natasha", "Rocket", "Steve", "Tchalla", "Ultron"]
+      ColossalCoaster.sorted_queue(queue).should eq expected
     end
   end
 end
--- src/colossal_coaster.cr
+++ src/colossal_coaster.cr
@@ -9,18 +9,28 @@ module ColossalCoaster
   end
 
   def self.find_my_friend(queue, friend_name)
-    raise "Please implement the ColossalCoaster.find_my_friend method"
+    # raise "Please implement the ColossalCoaster.find_my_friend method"
+    queue.index(friend_name) || -1
   end
 
   def self.add_with_friends(queue, friend_name, person_name)
-    raise "Please implement the ColossalCoaster.add_with_friends method"
+    # raise "Please implement the ColossalCoaster.add_with_friends method"
+    friend_index = find_my_friend(queue, friend_name)
+    if friend_index == 0
+      [person_name] + queue
+    else
+      queue[0..friend_index - 1] + [person_name] + queue[friend_index...]
+    end
   end
 
   def self.remove_from_queue(queue, person_name)
-    raise "Please implement the ColossalCoaster.remove_from_queue method"
+    # raise "Please implement the ColossalCoaster.remove_from_queue method"
+    queue.delete(person_name)
+    queue
   end
 
   def self.sorted_queue(queue)
-    raise "Please implement the ColossalCoaster.sorted_queue method"
+    # raise "Please implement the ColossalCoaster.sorted_queue method"
+    queue.sort
   end
 end
