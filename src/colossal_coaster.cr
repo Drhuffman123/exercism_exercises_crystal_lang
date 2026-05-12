05577ddceb50818f057e62bbb5a1f59866716cee
daniel huffman <drhuffman123@gmail.com>
2026-05-12 19:18:52 -0400

    comment cleanup

--- src/colossal_coaster.cr
+++ src/colossal_coaster.cr
@@ -1,6 +1,5 @@
 module ColossalCoaster
   def self.add_to_queue(express_queue, normal_queue, express, person_name)
-    # raise "Please implement the ColossalCoaster.add_to_queue method"
     if express == 1
       express_queue << person_name
     else
@@ -9,12 +8,10 @@ module ColossalCoaster
   end
 
   def self.find_my_friend(queue, friend_name)
-    # raise "Please implement the ColossalCoaster.find_my_friend method"
     queue.index(friend_name) || -1
   end
 
   def self.add_with_friends(queue, friend_name, person_name)
-    # raise "Please implement the ColossalCoaster.add_with_friends method"
     friend_index = find_my_friend(queue, friend_name)
     if friend_index == 0
       [person_name] + queue
@@ -24,13 +21,11 @@ module ColossalCoaster
   end
 
   def self.remove_from_queue(queue, person_name)
-    # raise "Please implement the ColossalCoaster.remove_from_queue method"
     queue.delete(person_name)
     queue
   end
 
   def self.sorted_queue(queue)
-    # raise "Please implement the ColossalCoaster.sorted_queue method"
     queue.sort
   end
 end
