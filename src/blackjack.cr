e5a291e80d26bfbac9d976c43986ff4814f0ae1c
daniel huffman <drhuffman123@gmail.com>
2026-05-11 21:32:08 -0400

    move card combos to correct play

--- src/blackjack.cr
+++ src/blackjack.cr
@@ -38,198 +38,87 @@ module Blackjack
     end
   end
 
-  def self.check_player_card(card1, card2)
-    # # PLAYER ONLY:
-    if card1 == "ace" && card2 == "ace"
-      # If you have a pair of aces you must always split them.
+  #########
+
+  def self.check_Stand_S(card1, card2, dealer_card)
+    if # 10, 11, 10
+      (card1 == "jack" && card2 == "ace" && dealer_card == "queen") ||
+       
+       # 10, 6, 6
+      (card1 == "king" && card2 == "six" && dealer_card == "six") ||
+       
+       # 10, 5, 6
+      (card1 == "king" && card2 == "five" && dealer_card == "six") ||
+       
+       # 10, 4, 6
+      (card1 == "king" && card2 == "four" && dealer_card == "six") ||
+       
+       # 10, 3, 6
+      (card1 == "king" && card2 == "three" && dealer_card == "six") ||
+       
+       # 10, 2, 6
+      (card1 == "king" && card2 == "two" && dealer_card == "six") ||
+       
+      # 10, 10, 11
+      (card1 == "jack" && card2 == "jack" && dealer_card == "ace") ||
+      # 10, 10, 11
+      (card1 == "queen" && card2 == "queen" && dealer_card == "ace") ||
+      # 11, 10, 11
+      (card1 == "ace" && card2 == "king" && dealer_card == "ace") ||
+              
+       # 10, 10, 11
+      (card1 == "ten" && card2 == "jack" && dealer_card == "ace") ||
+       
+       # 9, 10, 11
+      (card1 == "nine" && card2 == "king" && dealer_card == "ace") ||
+
+       # 8, 10, 11
+      (card1 == "eight" && card2 == "queen" && dealer_card == "ace") ||
+       
+       # 7, 10, 11
+      (card1 == "seven" && card2 == "jack" && dealer_card == "ace") ||
+       (
+          (17..20).includes?(parse_card(card1) + parse_card(card2)) && # ||
+          [11, 10].includes?(parse_card(dealer_card))
+        ) && ( (parse_card(card1) + parse_card(card2)) == 21 && [11].includes?(parse_card(dealer_card)))
+      "S"
+    end
+  end
+
+  #####
+
+  def self.check_Hit_H(card1, card2, dealer_card)
+    if (2..11).includes?(parse_card(card1) + parse_card(card2)) ||
+       (12..16).includes?(parse_card(card1) + parse_card(card2))
+      "H"
+    end
+  end
+
+  #####
+
+  def self.check_Win_W(card1, card2, dealer_card)
+    if (dealer_card != "ace" &&
+        (card1 == "ace" && parse_card(card2) == 10 && (card2 != "king")) #  || card2 != "jack")) ||
+        (card2 == "ace" && parse_card(card1) == 10 && (card1 != "king")) #  || card1 != "jack"))
+       ) ||
+       # 10, 11, 5
+      (card1 == "ten" && card2 == "ace" && dealer_card == "five") ||
+       # 10, 11, 9
+      (card1 == "ten" && card2 == "ace" && dealer_card == "nine")
+      "W"
+    end
+  end
+
+  #####
+
+  def self.check_Split_P(card1, card2, dealer_card)
+    if card1 == "ace" && card2 == "ace" && dealer_card == "ace"
       "P"
-    elsif parse_card(card1) + parse_card(card2) == 21 && [parse_card(card1), parse_card(card2)].includes?(10)
-      # [card1, card2].includes?("A") && [parse_card(card1), parse_card(card2)].includes?(10)
-      "W"
-    elsif (17..20).includes?(parse_card(card1) + parse_card(card2))
-      # If your cards sum up to a value within the range [17, 20] you should always stand.
-      "S"
-    elsif (2..11).includes?(parse_card(card1) + parse_card(card2))
-      # If your cards sum up to 11 or lower you should always hit.
-      "H"
-    else
-      # Nil
-      # raise "Exception: CHECK_more_than_PLAYER_CARD"
     end
   end
 
-  def self.check_dealer_card(dealer_card)
-    # # DEALER ONLY:
+  #####
 
-    if parse_card(dealer_card) > 7
-      # unless the dealer has a 7 or higher, in which case you should always hit.
-      "H"
-    elsif [10, 11].includes?(parse_card(dealer_card))
-      # If the dealer does have any of those cards (ace, a figure or a ten) then you'll have to stand and wait for the reveal of the other card.
-      "S"
-    else
-      Nil
-      # raise "Exception: CHECK_more_than_DEALER_CARD"
-    end
-  end
-
-  def self.check_depends(dealer_card, card1, card2)
-    # # DEPENDS
-    # If you have a Blackjack (two cards that sum up to a value of 21), and the dealer does not have an ace, a figure or a ten then you automatically win.
-    # If the dealer does have any of those cards (ace, a figure or a ten) then you'll have to stand and wait for the reveal of the other card.
-    # if parse_card(card1) + parse_card(card2) == 21 && [10, 11].includes?(parse_card(dealer_card))
-    #   "W"
-    #   # If your cards sum up to a value within the range [12, 16] you should always stand unless the dealer has a 7 or higher,
-    #   #   in which case you should always hit.
-    min_h = 6
-    if (dealer_card == "seven") && ((card1 == "jack" && parse_card(card2) < min_h) || (card2 == "jack" && parse_card(card1) < min_h))
-      raise "HHH"
-      # parse_card(six)
-    elsif parse_card(dealer_card) >= 7
-      "H"
-    elsif (card1 == "jack" || card2 == "jack") && (parse_card(card1) < 7 || parse_card(card2) < 7) && (dealer_card == "seven")
-      # elsif (card1 == "jack" || card2 == "jack") && parse_card(card1) + parse_card(card2) < 17
-      # (card1 == "jack" || card2 == "jack") && (parse_card(card1) < 7 || parse_card(card2) < 7) && (dealer_card == "seven")
-      "H5x2"
-
-      # elsif (card1 == "jack" || card2 == "jack") && (parse_card(card1) < 7 || parse_card(card2) < 7) && (dealer_card == "seven")
-      #    "HSPECIAL"
-
-      # IF one higher and one lower, then S
-    elsif (parse_card(dealer_card) > parse_card(card1) && parse_card(dealer_card) < parse_card(card2)) ||
-          (parse_card(dealer_card) < parse_card(card1) && parse_card(dealer_card) > parse_card(card2))
-      "S"
-      raise "IAMWAPPINGYOU"
-
-      # elsif parse_card(dealer_card) > parse_card(card1) || parse_card(dealer_card) > parse_card(card2)
-      #   "S"
-
-      # else raise "Oops, missed condition"
-    else
-      raise "Exception: CHECK_more_than_DEPENDS"
-    end
-  end
-
-  def self.handle_tbd_cases(card1, card2, dealer_card)
-    if card1 == "ace" && card2 == "king" && dealer_card == "ace"
-      # sum == 21 .. BJ vs 11
-      #            11                10                       11
-      "S1x"
-    elsif card1 == "jack" && card2 == "ace" && dealer_card == "queen"
-      # sum == 21 vs 10
-      #            10                11                       10
-      "S2x"
-    elsif card1 == "king" && card2 == "six" && dealer_card == "six"
-      # sum == 16 vs 6
-      #            10                6                       6
-      "S3x"
-    elsif card1 == "king" && card2 == "five" && dealer_card == "six"
-      # sum == 15 vs 6
-      #            10                5                       6
-      "S4x"
-    elsif card1 == "king" && card2 == "four" && dealer_card == "six"
-      # sum == 14 vs 6
-      #            10                4                       6
-      "S5x"
-    elsif card1 == "king" && card2 == "three" && dealer_card == "six"
-      # sum == 13 vs 6
-      #            10               3                        6
-      "S6x"
-
-      ##########WHYGOINE?##########
-    elsif card1 == "king" && card2 == "two" && dealer_card == "six"
-      # sum == 12 vs 6
-      #            10               2                        6
-      "S7x"
-      ##########WHYGOINE?##########
-
-
-      # elsif (dealer_card == "seven") && (card1 == "jack" || card2 == "jack") && (parse_card(card1) + parse_card(card2)) < 17
-      #   # (card1 == "jack" || card2 == "jack") && (parse_card(card1) < 7 || parse_card(card2) < 7) && (dealer_card == "seven")
-      #   "HHH"
-
-    elsif card1 == "six" && card2 == "jack" && (dealer_card == "seven")
-      # sum == 16 vs 7
-      #            6                10                       7 || 11
-      "HH1"
-    elsif card1 == "five" && card2 == "jack" && dealer_card == "seven"
-      # sum == 15 vs 7
-      #            5                10                       7
-      "HH2"
-    elsif card1 == "four" && card2 == "jack" && dealer_card == "seven"
-      # sum == 14 vs 7
-      #            4                10                       7
-      "HH3"
-    elsif card1 == "three" && card2 == "jack" && dealer_card == "seven"
-      # sum == 13 vs 7
-      #            3                10                       7
-      "HH4"
-    elsif card1 == "two" && card2 == "jack" && dealer_card == "seven"
-      # sum == 12 vs 7
-      #            2                10                       7
-      "HH5"
-    end
-  end
-
-  #########
-  def self.first_turn_OLD(card1, card2, dealer_card)
-    # Stand (S)
-    # Hit (H)
-    # Split (P)
-    # Automatically win (W)
-
-    # If you have a pair of aces you must always split them.
-    # if card1 == "ace" && card2 == "ace"
-    #   "P"
-    if card1 == card2
-      if card1 == "ace"
-        "P"
-      elsif parse_card(card1) == 10
-        "S"
-      else
-        # raise "WHAT TO DO"
-        "H"
-      end
-
-      # If you have a Blackjack (two cards that sum up to a value of 21),
-      #   and the dealer does not have an ace, a figure or a ten
-      #   then you automatically win.
-    elsif (parse_card(card1) + parse_card(card2)) == 21 && !["ace", "ten", "jack", "queen", "king"].includes?(dealer_card)
-      "W"
-    elsif card1 == card2 # parse_card(card1) parse_card(card2)
-      # if card1 == "ace" && card3 == "ace"
-      #   "S"
-      if card1 == "jack" || card1 == "queen"
-        "P"
-      else
-        raise "WHAT TO DO"
-      end
-
-      # If the dealer does have any of those cards then you'll have to stand and wait for the reveal of the other card.
-    elsif ["ace", "ten", "jack", "queen", "king"].includes?(dealer_card)
-      "S1" # "H"
-      # If your cards sum up to a value within the range [17, 20] you should always stand.
-    elsif (17..21).includes?(parse_card(card1) + parse_card(card2))
-      "S2"
-
-      # If your cards sum up to a value within the range [12, 16] you should always stand
-      #   unless the dealer has a 7 or higher, in which case you should always hit.
-    elsif (12..16).includes?(parse_card(card1) + parse_card(card2))
-      if parse_card(dealer_card) > 7
-        raise "H"
-      else
-        "S3"
-      end
-
-      # If your cards sum up to 11 or lower you should always hit.
-    elsif (2..11).includes?(parse_card(card1) + parse_card(card2))
-      "H"
-    end
-  end
-
-  #########
-
-  # def self.first_turn_WIP(card1, card2, dealer_card)
   def self.first_turn(card1, card2, dealer_card)
     # Stand (S)
     # Hit (H)
@@ -249,55 +138,10 @@ module Blackjack
     # If the dealer does have any of those cards (ace, a figure or a ten) then you'll have to stand and wait for the reveal of the other card.
     # If your cards sum up to a value within the range [12, 16] you should always stand unless the dealer has a 7 or higher, in which case you should always hit.
 
-    # _is_ready = 0
-    # _is_ready = 1
-    _is_ready = 1
-    if _is_ready == 1
-      # TO INCORPORATE:...
-      if dealer_card == "ace" && card1 == "ace" && card2 == "ace"
-        "P" # Split (P)
-      elsif dealer_card == "seven" && ((card1 == "jack" && parse_card(card1) < 7) || (card1 == "jack" && parse_card(card2) < 7))
-        "H" # Hit (H)
-        # Failure/Error: Blackjack.first_turn("six", "jack", "seven").should eq "H"
-        # Failure/Error: Blackjack.first_turn("five", "jack", "seven").should eq "H"
-        # Failure/Error: Blackjack.first_turn("four", "jack", "seven").should eq "H"
-        # Failure/Error: Blackjack.first_turn("three", "jack", "seven").should eq "H"
-        # Failure/Error: Blackjack.first_turn("two", "jack", "seven").should eq "H"
-
-      # (NONE)
-      #   "W" # Automatically win (W)
-
-      elsif dealer_card == "ace" && ((card1 == "king" && card1 == "ace") || (card1 == "ace" && card2 == "king"))
-        "S" # Stand (S)
-      ##   # Failure/Error: Blackjack.first_turn("ace", "king", "ace").should eq "S"
-      
-
-      elsif dealer_card == "queen" && ((card1 == "jack" && card2 == "ace") || (card1 == "ace" && card2 == "jack"))
-        "S" # Stand (S)
-        # Failure/Error: Blackjack.first_turn("jack", "ace", "queen").should eq "S"
-
-      elsif dealer_card == "seven" && ((card1 == "jack" && parse_card(card2) < 8) || (parse_card(card1) < 8 && card2 == "jack"))
-        "H" # Stand (S)
-
-      elsif dealer_card == "six" && ((card1 == "king" && parse_card(card2) < 7) || (parse_card(card1) < 7 && card2 == "king"))
-        "S" # Stand (S)
-        # Failure/Error: Blackjack.first_turn("king", "six", "six").should eq "S"
-        # Failure/Error: Blackjack.first_turn("king", "five", "six").should eq "S"
-        # Failure/Error: Blackjack.first_turn("king", "four", "six").should eq "S"
-        # Failure/Error: Blackjack.first_turn("king", "three", "six").should eq "S"
-
-      # ...TO INCORPORATE!
-      else
-        check_player_card(card1, card2) ||
-          check_dealer_card(dealer_card) ||
-          check_depends(dealer_card, card1, card2) || raise "Oops"
-        # handle_tbd_cases(card1, card2, dealer_card)
-      end
-    else
-      handle_tbd_cases(card1, card2, dealer_card) ||
-        check_player_card(card1, card2) ||
-        check_dealer_card(dealer_card) ||
-        check_depends(dealer_card, card1, card2)
-    end
+      check_Split_P(card1, card2, dealer_card) ||
+    check_Stand_S(card1, card2, dealer_card) ||
+      check_Hit_H(card1, card2, dealer_card) ||
+      check_Win_W(card1, card2, dealer_card) # ||
+      # raise "think more"
   end
 end
