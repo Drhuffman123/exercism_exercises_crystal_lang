88c84a833220e8bc44881a3b1e415b974f4db873
daniel huffman <drhuffman123@gmail.com>
2026-05-12 22:03:41 -0400

    WIP re TheFarm

--- spec/the_farm_spec.cr
+++ spec/the_farm_spec.cr
@@ -9,47 +9,82 @@ describe TheFarm do
     end
 
     it "Test 2" do
+      food = FodderCalculator.new(60.5, 1.3)
+      TheFarm.divide_food(food, 5).should eq 15.73
     end
   end
 
   context "TASK 2" do
     it "Test 3" do
+      food = FodderCalculator.new(nil, 100)
+      TheFarm.divide_food(food, 10).should eq 0
     end
 
     it "Test 4" do
-    end
-
-    it "Test 5" do
+      food = FodderCalculator.new(1000, nil)
+      TheFarm.divide_food(food, 10).should eq 0
     end
   end
 
   context "TASK 3" do
+    it "Test 5" do
+      expect_raises(Exception, "Number of cows must be greater than 0") do
+        TheFarm.validate_input_and_divide_food(FodderCalculator.new(10, 1), -10)
+      end
+    end
+
     it "Test 6" do
+      expect_raises(Exception, "Number of cows must be greater than 0") do
+        TheFarm.validate_input_and_divide_food(FodderCalculator.new(10, 1), 0)
+      end
     end
 
     it "Test 7" do
+      food = FodderCalculator.new(100, 1)
+      TheFarm.validate_input_and_divide_food(food, 10).should eq 10
     end
 
     it "Test 8" do
+      food = FodderCalculator.new(60.5, 1.3)
+      TheFarm.validate_input_and_divide_food(food, 5).should eq 15.73
+    end
+
+    it "Test 9" do
+      food = FodderCalculator.new(nil, 100)
+      TheFarm.validate_input_and_divide_food(food, 10).should eq 0
+    end
+
+    it "Test 10" do
+      food = FodderCalculator.new(1000, nil)
+      TheFarm.validate_input_and_divide_food(food, 10).should eq 0
     end
   end
 
   context "TASK 4" do
-    it "Test 9" do
-    end
-
-    it "Test 10" do
-    end
-
     it "Test 11" do
+      TheFarm.validate_number_of_cows(80).should eq nil
     end
-  end
 
-  context "TASK 5" do
     it "Test 12" do
+      TheFarm.validate_number_of_cows(1).should eq nil
     end
 
     it "Test 13" do
+      expect_raises(InvalidCowsError, "-20 cows are invalid: there are no negative cows") do
+        TheFarm.validate_number_of_cows(-20)
+      end
+    end
+
+    it "Test 14" do
+      expect_raises(InvalidCowsError, "-1 cows are invalid: there are no negative cows") do
+        TheFarm.validate_number_of_cows(-1)
+      end
+    end
+
+    it "Test 15" do
+      expect_raises(InvalidCowsError, "0 cows are invalid: no cows don't need food") do
+        TheFarm.validate_number_of_cows(0)
+      end
     end
   end
 end
--- src/the_farm.cr
+++ src/the_farm.cr
@@ -7,36 +7,48 @@ end
 class FodderCalculator
   # property num : Int32
   # property ratio : Float64
-  property total_food : Int32 | Float64 # | Nil
-  property animals : Int32 | Float64 # | Nil
+  property total_food : Int32 | Float64 | Nil
+  property animals : Int32 | Float64 | Nil
     
-  def initialize(@total_food : Int32 | Float64, @animals : Int32 | Float64) #  | Nil
-    # raise "oops" if @animals.nil? # || @animals.to_f < 1
-    @total_food / @animals
+  def initialize(total_food  : Int32 | Float64 | Nil, animals : Int32 | Float64 | Nil)
+    @total_food = total_food
+    @animals = animals
+
+    if @total_food.nil? || @animals.nil?
+      raise "oops"
+    end
   end
     
-  def fodder_amount!()
-    @total_food
+  def fodder_amount!
+    if !@total_food.nil?
+      # @total_food.to_f64
+      Float64.new("0#{@total_food}")
+    else
+      0
+    end
   end
 
-  def fattening_factor!()
-    @ratio
+  def fattening_factor!
+    # TBD: @ratio
   end
 end
 
 module TheFarm
 
-  def self.divide_food(food : FodderCalculator? = nil, number_of_cows = 0)
+  # ? = nil
+  def self.divide_food(food : FodderCalculator | Nil, number_of_cows : Int32 | Nil = 0)
     if number_of_cows.nil? || number_of_cows.to_f <= 0
       raise InvalidCowsError.new("number_of_cows MUST be > 0")
+    elsif food.nil?
+      raise InvalidCowsError.new("food MUST be > 0")
     else
-      food.fodder_amount! / number_of_cows
+      food.fodder_amount!.to_f / number_of_cows.to_f
     end
   end
 
   def self.validate_input_and_divide_food(food : FodderCalculator, number_of_cows : Int32)
     if number_of_cows > 0
-      divide_food
+      divide_food(food, number_of_cows)
     else
       raise "Number of cows must be greater than 0"
     end
