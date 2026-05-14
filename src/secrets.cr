37c2e4ee43746f67feb6ab89f71f86302c19fa71
daniel huffman <drhuffman123@gmail.com>
2026-05-14 17:37:29 -0400

    initial Secrets src and spec

---
+++ src/secrets.cr
@@ -0,0 +1,21 @@
+module Secrets
+  def self.shift_back(value : UInt8, number_of_places : UInt8) : UInt8
+    # raise "Please implement the Secrets.shift_back method"
+    0.to_u8
+  end
+
+  def self.apply_mask(value : UInt8, mask : UInt8) : UInt8
+    # raise "Please implement the Secrets.apply_mask method"
+    0.to_u8
+  end
+
+  def self.set_bits(value1 : UInt8, value2 : UInt8) : UInt8
+    # raise "Please implement the Secrets.set_bits method"
+    0.to_u8
+  end
+
+  def self.reverse_xor(value : UInt8, agreed_value : UInt8) : UInt8
+    # raise "Please implement the Secrets.reverse_xor method"
+    0.to_u8
+  end
+end
