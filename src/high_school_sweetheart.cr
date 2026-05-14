3c98f5c36c31f56e64dffb41c6063af329c0bea7
daniel huffman <drhuffman123@gmail.com>
2026-05-13 21:22:43 -0400

    HighSchoolSweetheart

---
+++ src/high_school_sweetheart.cr
@@ -0,0 +1,25 @@
+class HighSchoolSweetheart
+  def self.clean_up_name(name)
+    name.gsub("-", " ").strip
+  end
+
+  def self.first_letter(name)
+    names = clean_up_name(name).split
+
+    name_first = names[0]
+
+    name_first_letter = name_first[0]
+  end
+
+  def self.initial(name)
+    names : Array(String) = clean_up_name(name).split
+
+    first_name = names[0]
+
+    first_name[0].upcase + "."
+  end
+
+  def self.pair(name1, name2)
+    "❤ #{initial(name1)}  +  #{initial(name2)} ❤"
+  end
+end
