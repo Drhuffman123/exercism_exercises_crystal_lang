24c5f5590f09cafc3fd8ea6db59be614ab484e65
daniel huffman <drhuffman123@gmail.com>
2026-05-13 22:08:39 -0400

    comment initial raises

--- src/foto_fusionist.cr
+++ src/foto_fusionist.cr
@@ -1,17 +1,21 @@
 class FotoFusionist
   def self.binary_to_int(binary : String) : Int32
-    raise "Please implement the FotoFusionist.binary_to_int method"
+    # raise "Please implement the FotoFusionist.binary_to_int method"
+    0
   end
 
   def self.least_significant_bit(number : Int32) : Int32
-    raise "Please implement the FotoFusionist.least_significant_bit method"
+    # raise "Please implement the FotoFusionist.least_significant_bit method"
+    0
   end
 
   def self.hex_value(number : Int32) : String
-    raise "Please implement the FotoFusionist.hex_value method"
+    # raise "Please implement the FotoFusionist.hex_value method"
+    ""
   end
 
   def self.hex_value_from_rgb(red : Int32, green : Int32, blue : Int32) : String
-    raise "Please implement the FotoFusionist.hex_value_from_rgb method"
+    # raise "Please implement the FotoFusionist.hex_value_from_rgb method"
+    ""
   end
 end
