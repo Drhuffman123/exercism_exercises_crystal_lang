ca3c257b346258728a1b45d96c15f89b22908da0
daniel huffman <drhuffman123@gmail.com>
2026-05-14 16:45:32 -0400

    FotoFusionist

--- src/foto_fusionist.cr
+++ src/foto_fusionist.cr
@@ -1,16 +1,15 @@
+require "big"
+
 class FotoFusionist
   def self.binary_to_int(binary : String) : Int32
-    # raise "Please implement the FotoFusionist.binary_to_int method"
-    binary.to_i(8)
+    BigInt.new(binary, base: 2).to_i
   end
 
   def self.least_significant_bit(number : Int32) : Int32
-    # raise "Please implement the FotoFusionist.least_significant_bit method"
     number.bit(0)
   end
 
   def self.hex_value(number : Int32) : String
-    # raise "Please implement the FotoFusionist.hex_value method"
     if number <= 7
       "0" + number.to_s(16)
     else
@@ -19,8 +18,6 @@ class FotoFusionist
   end
 
   def self.hex_value_from_rgb(red : Int32, green : Int32, blue : Int32) : String
-    # raise "Please implement the FotoFusionist.hex_value_from_rgb method"
-    ""
-    red.to_s(16) + green.to_s(16) + blue.to_s(16)
+    hex_value(red) + hex_value(green) + hex_value(blue)
   end
 end
