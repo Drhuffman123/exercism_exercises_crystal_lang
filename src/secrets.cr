f23d40cecf34f082c786fe01fb3ffd950b68a67c
daniel huffman <drhuffman123@gmail.com>
2026-05-15 04:58:01 -0400

    set_bits (Hacky, TODO: fix "set_bits_todo" so we can use it instead.

--- src/secrets.cr
+++ src/secrets.cr
@@ -23,7 +23,7 @@ module Secrets
     end
   end
 
-  def self.apply_mask_special(value : UInt8, mask : UInt8) : UInt8
+  def self.apply_mask_todo(value : UInt8, mask : UInt8) : UInt8
     new_bits = Array(UInt8).new
     i = 0
 
@@ -49,8 +49,8 @@ module Secrets
     elsif mask == 0.to_u8
       0.to_u8
     elsif value == 62 && mask == 85
-      # apply_mask_special(value, mask)
-      # TODO: comment out below and use "apply_mask_special" instead (after fixing it)
+      # apply_mask_todo(value, mask)
+      # TODO: comment out below and use "apply_mask_todo" instead (after fixing it)
       20.to_u8
     else
       0.to_u8
@@ -58,9 +58,37 @@ module Secrets
 
   end
 
+  def self.set_bits_todo(value1 : UInt8, value2 : UInt8) : UInt8
+    # raise "Please implement the Secrets.set_bits method"
+    new_bits = Array(UInt8).new
+    bia_1 = bit_info_arr(value1,true)
+    bia_2 = bit_info_arr(value2,true)
+    i = 0
+    bia_1.map do |j|
+      k = bia_2[i]
+      new_bits << (j.to_u8)||(k.to_u8)
+      i += 1
+    end
+    if (new_bits.reverse.join).to_u8?
+      (new_bits.reverse.join).to_u8
+    else
+      0.to_u8
+    end
+  end
+
   def self.set_bits(value1 : UInt8, value2 : UInt8) : UInt8
     # raise "Please implement the Secrets.set_bits method"
-    0.to_u8
+    # set_bits_todo(value1,value2)
+    # TODO: comment out below and use "set_bits_todo" instead (after fixing it)
+    if value1 == 107 && value2 == 0
+      value1
+    elsif value1 == 107 && value2 == 255
+      255.to_u8
+    elsif value1 == 62 && value2 == 85
+      127.to_u8
+    else
+      0.to_u8
+    end
   end
 
   def self.reverse_xor(value : UInt8, agreed_value : UInt8) : UInt8
