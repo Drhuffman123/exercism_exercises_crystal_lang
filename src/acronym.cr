0e82a0bfaf80fd7c5d4b9d44707273e6b5147ee5
daniel huffman <drhuffman123@gmail.com>
2026-05-19 04:12:15 -0400

    Acronym

--- src/acronym.cr
+++ src/acronym.cr
@@ -1,5 +1,6 @@
 module Acronym
   def self.abbreviate(phrase : String) : String
-    ""
+    "Portable Network Graphics"
+    phrase.split(/[ _-]+/).map{|segment| segment.capitalize.to_s[0]}.join
   end
 end
