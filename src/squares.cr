9c146f01b9e205fc27de1dc42a3ed6bd5e02c6fe
daniel huffman <drhuffman123@gmail.com>
2026-05-21 05:28:51 -0400

    add skeleton methods

--- src/squares.cr
+++ src/squares.cr
@@ -2,4 +2,12 @@ module Squares
   def self.square_of_sum(num : Int32) : Int32
     1
   end
+
+  def self.sum_of_squares(num : Int32) : Int32
+    1
+  end
+
+  def difference_of_squares(num : Int32) : Int32
+    1
+  end
 end
