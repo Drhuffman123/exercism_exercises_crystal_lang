491204eebb0de9005c196cce08bdd1cfa3e6b29c
daniel huffman <drhuffman123@gmail.com>
2026-05-23 01:38:08 -0400

    un-pend the specs

--- spec/hamming_spec.cr
+++ spec/hamming_spec.cr
@@ -3,49 +3,49 @@ require "../src/hamming.cr"
 
 describe "Hamming" do
   context "distance eq 0" do
-    pending "Test 1" do
+    it "Test 1" do
       Hamming.distance("", "").should eq(0)
     end
 
-    pending "Test 2" do
+    it "Test 2" do
       Hamming.distance("A", "A").should eq(0)
     end
 
-    pending "Test 4" do
+    it "Test 4" do
       Hamming.distance("GGACTGAAATCTG", "GGACTGAAATCTG").should eq(0)
     end
   end
 
   context "distance eq positive int" do
-    pending "Test 3" do
+    it "Test 3" do
       Hamming.distance("G", "T").should eq(1)
     end
 
-    pending "Test 5" do
+    it "Test 5" do
       Hamming.distance("GGACGGATTCTG", "AGGACGGATTCT").should eq(9)
     end
   end
 
   context "distance raises" do
-    pending "Test 6" do
+    it "Test 6" do
       expect_raises(ArgumentError) do
         Hamming.distance("AATG", "AAA")
       end
     end
 
-    pending "Test 7" do
+    it "Test 7" do
       expect_raises(ArgumentError) do
         Hamming.distance("ATA", "AGTG")
       end
     end
 
-    pending "Test 8" do
+    it "Test 8" do
       expect_raises(ArgumentError) do
         Hamming.distance("", "G")
       end
     end
 
-    pending "Test 9" do
+    it "Test 9" do
       expect_raises(ArgumentError) do
         Hamming.distance("G", "")
       end
