e71cf297645efd2986e8588766d53d20ac511e74
daniel huffman <drhuffman123@gmail.com>
2026-05-29 03:34:23 -0400

    Add specs for ResistorColorDuo

--- spec/resistor_color_duo_spec.cr
+++ spec/resistor_color_duo_spec.cr
@@ -6,27 +6,27 @@ describe "ResistorColorDuo" do
     ResistorColorDuo.value(["brown", "black"]).should eq(10)
   end
 
-  pending "Blue and grey" do
+  it "Blue and grey" do
     ResistorColorDuo.value(["blue", "grey"]).should eq(68)
   end
 
-  pending "Yellow and violet" do
+  it "Yellow and violet" do
     ResistorColorDuo.value(["yellow", "violet"]).should eq(47)
   end
 
-  pending "White and red" do
+  it "White and red" do
     ResistorColorDuo.value(["white", "red"]).should eq(92)
   end
 
-  pending "Orange and orange" do
+  it "Orange and orange" do
     ResistorColorDuo.value(["orange", "orange"]).should eq(33)
   end
 
-  pending "Ignore additional colors" do
+  it "Ignore additional colors" do
     ResistorColorDuo.value(["green", "brown", "orange"]).should eq(51)
   end
 
-  pending "Black and brown, one-digit" do
+  it "Black and brown, one-digit" do
     ResistorColorDuo.value(["black", "brown"]).should eq(1)
   end
 end
