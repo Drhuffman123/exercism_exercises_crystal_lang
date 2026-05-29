ad17d1fcaf2c9149fbe58469c1204d0c9d0e6ad8
daniel huffman <drhuffman123@gmail.com>
2026-05-29 04:37:37 -0400

    add simple logging

--- src/rotational_cipher.cr
+++ src/rotational_cipher.cr
@@ -1,6 +1,8 @@
 module RotationalCipher
   def self.rotate(plaintext : String, key : Number) : String
     # Write your code for the 'Rotational Cipher' exercise in this file.
-    plaintext
+    result = plaintext
+    puts "\nLOG:: plaintext: #{plaintext}, key: #{key}, result: #{result}\n"
+    result
   end
 end
