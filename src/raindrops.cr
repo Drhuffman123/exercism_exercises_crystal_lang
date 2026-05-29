5013a8f64ed2b878c9d8f2ad26462cabe8ae5c54
daniel huffman <drhuffman123@gmail.com>
2026-05-29 01:54:54 -0400

    solved Raindrops

--- src/raindrops.cr
+++ src/raindrops.cr
@@ -1,6 +1,19 @@
 class Raindrops
+  def self.err_if_div_evenly_by(num, quotient, drop)
+    puts "\n* num: #{num}, quotient: #{quotient}, drop: #{drop}, (num/quotient).to_i: #{(num/quotient).to_i}, (num.to_f/quotient).to_s: #{(num.to_f/quotient).to_s}"
+    if (num/quotient).to_i == (num/quotient) # .to_f # .to_i
+      drop
+    else
+      ""
+    end
+  end
+
   def self.convert(num : Int32) : String
-    # Write your code for the 'Raindrops' exercise in this file.
-    "ERROR"
+    err_str = err_if_div_evenly_by(num, 3, "Pling") + err_if_div_evenly_by(num, 5, "Plang") + err_if_div_evenly_by(num, 7, "Plong")
+    if err_str != ""
+      err_str
+    else
+      num.to_s
+    end
   end
 end
