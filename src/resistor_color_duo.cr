4171390aa627ef5a8f56938f39fd66aa9d890719
daniel huffman <drhuffman123@gmail.com>
2026-05-29 03:40:47 -0400

    crystal tool format

--- src/resistor_color_duo.cr
+++ src/resistor_color_duo.cr
@@ -2,6 +2,6 @@ require "./resistor_color.cr"
 
 module ResistorColorDuo
   def self.value(values : Array(String)) : Number
-    values[0..1].map {|val| ResistorColor.color_code(val)}.join.to_i
+    values[0..1].map { |val| ResistorColor.color_code(val) }.join.to_i
   end
 end
