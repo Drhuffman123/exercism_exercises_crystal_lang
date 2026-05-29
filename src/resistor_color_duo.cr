e8117e57bdccebc63d534cd136d2b92915678885
daniel huffman <drhuffman123@gmail.com>
2026-05-29 03:44:21 -0400

    put ResistorColor directly in ResistorColorDuo

--- src/resistor_color_duo.cr
+++ src/resistor_color_duo.cr
@@ -1,4 +1,24 @@
-require "./resistor_color.cr"
+module ResistorColor
+  def self.color_code(color : String) : Int32 | Nil
+    if colors.includes?(color)
+      color_indexes[color]
+    else
+      nil
+    end
+  end
+
+  def self.colors : Array
+    ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  end
+
+  def self.color_indexes : Hash
+    hash_colors = Hash(String, Int32).new { |hash, key| hash[key] = 0 }
+    colors.map_with_index do |color, index|
+      hash_colors[color] = index
+    end
+    hash_colors
+  end
+end
 
 module ResistorColorDuo
   def self.value(values : Array(String)) : Number
