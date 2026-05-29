19db1807ec783bfecfeb246e13fb7edf3affe9aa
daniel huffman <drhuffman123@gmail.com>
2026-05-29 02:39:20 -0400

    given starter file for ResistorColor

---
+++ src/resistor_color.cr
@@ -0,0 +1,10 @@
+module ResistorColor
+  # Write your code for the 'Resistor Color' exercise in this file.
+  def self.color_code(color : String) : Int32 | Nil
+    nil
+  end
+
+  def self.colors : Array
+    Array(String | Int32).new
+  end
+end
