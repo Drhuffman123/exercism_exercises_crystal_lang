37e668cb849372f232663d323774e5fb7df27e2c
daniel huffman <drhuffman123@gmail.com>
2026-05-31 10:25:05 -0400

    RnaComplement src and specs

--- src/rna_complement.cr
+++ src/rna_complement.cr
@@ -1,6 +1,23 @@
 class RnaComplement
+  def self.of_dna_one_strand(strand : Char) : Char
+    if strand == 'G'
+      'C'
+    elsif strand == 'C'
+      'G'
+    elsif strand == 'T'
+      'A'
+    elsif strand == 'A'
+      'U'
+    else
+      raise "bad strand"
+    end
+  end
+
   def self.of_dna(strand : String) : String
-    # Write your code for the 'Rna Transcription' exercise in this file.
-    "tbd"
+    if !strand.nil?
+      strand.chars.map { |char| of_dna_one_strand(char) }.join
+    else
+      ""
+    end
   end
 end
