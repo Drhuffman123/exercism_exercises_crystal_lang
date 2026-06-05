442aaf1df155f8e25fb19e98f19e6a17d5a9e4c7
daniel huffman <drhuffman123@gmail.com>
2026-06-04 21:01:54 -0400

    Darts .. WIP

--- spec/darts_spec.cr
+++ spec/darts_spec.cr
@@ -2,55 +2,63 @@ require "spec"
 require "../src/darts.cr"
 
 describe "Darts" do
-  it "Missed target" do
-    Darts.score(-9.0, 9.0).should eq(0)
+  context "10" do
+    it "On the inner circle" do
+      Darts.score(0.0, -1.0).should eq(10)
+    end
+
+    it "Exactly on center" do
+      Darts.score(0.0, 0.0).should eq(10)
+    end
+
+    it "Near the center" do
+      Darts.score(-0.1, -0.1).should eq(10)
+    end
+
+    it "Just within the inner circle" do
+      Darts.score(0.7, 0.7).should eq(10)
+    end
   end
 
-  pending "On the outer circle" do
-    Darts.score(0.0, 10.0).should eq(1)
+  context "5" do
+    it "On the middle circle" do
+      Darts.score(-5.0, 0.0).should eq(5)
+    end
+
+    it "Just outside the inner circle" do
+      Darts.score(0.8, -0.8).should eq(5)
+    end
+
+    it "Just within the middle circle" do
+      Darts.score(-3.5, 3.5).should eq(5)
+    end
+
+    it "Asymmetric position between the inner and middle circles" do
+      Darts.score(0.5, -4.0).should eq(5)
+    end
   end
 
-  pending "On the middle circle" do
-    Darts.score(-5.0, 0.0).should eq(5)
+  context "1" do
+    it "On the outer circle" do
+      Darts.score(0.0, 10.0).should eq(1)
+    end
+
+    it "Just outside the middle circle" do
+      Darts.score(-3.6, -3.6).should eq(1)
+    end
+
+    it "Just within the outer circle" do
+      Darts.score(-7.0, 7.0).should eq(1)
+    end
   end
 
-  pending "On the inner circle" do
-    Darts.score(0.0, -1.0).should eq(10)
-  end
+  context "0" do
+    it "Missed target" do
+      Darts.score(-9.0, 9.0).should eq(0)
+    end
 
-  pending "Exactly on center" do
-    Darts.score(0.0, 0.0).should eq(10)
-  end
-
-  pending "Near the center" do
-    Darts.score(-0.1, -0.1).should eq(10)
-  end
-
-  pending "Just within the inner circle" do
-    Darts.score(0.7, 0.7).should eq(10)
-  end
-
-  pending "Just outside the inner circle" do
-    Darts.score(0.8, -0.8).should eq(5)
-  end
-
-  pending "Just within the middle circle" do
-    Darts.score(-3.5, 3.5).should eq(5)
-  end
-
-  pending "Just outside the middle circle" do
-    Darts.score(-3.6, -3.6).should eq(1)
-  end
-
-  pending "Just within the outer circle" do
-    Darts.score(-7.0, 7.0).should eq(1)
-  end
-
-  pending "Just outside the outer circle" do
-    Darts.score(7.1, -7.1).should eq(0)
-  end
-
-  pending "Asymmetric position between the inner and middle circles" do
-    Darts.score(0.5, -4.0).should eq(5)
+    it "Just outside the outer circle" do
+      Darts.score(7.1, -7.1).should eq(0)
+    end
   end
 end
--- src/darts.cr
+++ src/darts.cr
@@ -1,5 +1,13 @@
 module Darts
   def self.score(x : Number, y : Number) : Number
-    # Write your code for the 'Darts' exercise in this file.
+    if Math.sqrt((x*y).abs) <= 1
+      10
+    elsif Math.sqrt((x*y).abs) <= 5
+      5
+    elsif Math.sqrt((x*y).abs) <= 10
+      1
+    else
+      0
+    end
   end
 end
