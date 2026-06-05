13956926f4f4d9f464c4598b003a8f61448d6ab6
daniel huffman <drhuffman123@gmail.com>
2026-06-04 20:05:11 -0400

    rename 'check_sides' to 'valid_sides?'; cleanup

--- src/triangle.cr
+++ src/triangle.cr
@@ -3,22 +3,16 @@ class Triangle
 
   def initialize(sides : Tuple(Int32 | Float64, Int32 | Float64, Int32 | Float64))
     @sides = sides
-    if !check_sides(sides)
-      raise ArgumentError.new("Side lengths are NOT valid!")
+    if !valid_sides?(sides)
+      raise ArgumentError.new("Side lengths do not make a valid triangle")
     end
   end
 
-  def check_sides(sides)
+  def valid_sides?(sides)
     side0 = sides[0]
     side1 = sides[1]
     side2 = sides[2]
-    valid_pairs = (side0 > 0 && side1 > 0 && side2 > 0) && (side0 + side1 >= side2) && (side1 + side2 >= side0) && (side2 + side0 >= side1)
-
-    # if !valid_pairs
-    #   raise ArgumentError.new("Side lengths are NOT valid!")
-    # else
-    #   valid_pairs
-    # end
+    (side0 > 0 && side1 > 0 && side2 > 0) && (side0 + side1 >= side2) && (side1 + side2 >= side0) && (side2 + side0 >= side1)
   end
 
   def equilateral?
