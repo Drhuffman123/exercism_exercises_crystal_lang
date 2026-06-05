947fd64b3b9a59060589bbd2d49cf017a6aeff5e
daniel huffman <drhuffman123@gmail.com>
2026-06-05 07:47:25 -0400

    trimmed TwoFer

--- src/two_fer.cr
+++ src/two_fer.cr
@@ -1,9 +1,5 @@
 module TwoFer
   def self.two_fer(name : String? = nil)
-    if name.nil?
-      "One for you, one for me."
-    else
-      "One for #{name}, one for me."
-    end
+    "One for #{name || "you"}, one for me."
   end
 end
