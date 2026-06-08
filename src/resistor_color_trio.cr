0ba68dd438862a2adcd592cf6adaa00d2c46b799
daniel huffman <drhuffman123@gmail.com>
2026-06-08 12:27:37 -0400

    Resistor cleanup

--- src/resistor_color_trio.cr
+++ src/resistor_color_trio.cr
@@ -12,26 +12,26 @@ module ResistorColorTrio
     val1 = values[1]
     band_scale = BAND_DIGITS[val0].to_s + BAND_DIGITS[val1].to_s
     if (band_scale.to_f/10).to_i == 0
-      [(band_scale.to_f/10).to_i,10]
+      [(band_scale.to_f/10).to_i, 10]
     else
-      [band_scale.to_i,1]
+      [band_scale.to_i, 1]
     end
   end
 
   def self.label_scale(values) : Array(String)
     label_digits_with_scale = label_digits(values)
-    BAND_SCALES[values[2]+label_digits_with_scale[1]].to_s
+    BAND_SCALES[values[2] + label_digits_with_scale[1]].to_s
   end
 
   def self.label(values : Array(String), scale = 1) : String
     val = 10*BAND_DIGITS[values[0]] + BAND_DIGITS[values[1]]
     index = values[2]
-    scale_color = values[2]
-    scale = if (val/10) - (val/10).to_i == 0
-        BAND_SCALES.values.to_a[index+1]
-      else
-        BAND_SCALES.values.to_a[index]
-      end
-    "#{val} #{scale}"
+    # scale_color = values[2]
+    scale_str = if (val/10) - (val/10).to_i == 0
+                  BAND_SCALES.values.to_a[index + 1]
+                else
+                  BAND_SCALES.values.to_a[index]
+                end
+    "#{val} #{scale_str}"
   end
 end
