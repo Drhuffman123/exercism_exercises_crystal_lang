796b68c2b01ec62f3828f0432eb29bd6471ad219
daniel huffman <drhuffman123@gmail.com>
2026-06-11 23:11:41 -0400

    HighScores src and spec (reorganized)

--- src/high_scores.cr
+++ src/high_scores.cr
@@ -1,3 +1,19 @@
 class HighScores
-  # Write your code for the 'HighScores' exercise in this file.
+  property scores : Array(Int32)
+
+  def initialize(scores : Array(Int32))
+    @scores = scores
+  end
+
+  def latest
+    @scores[-1]
+  end
+
+  def personal_best
+    @scores.max
+  end
+
+  def personal_top_three
+    @scores.sort.reverse![0..2]
+  end
 end
