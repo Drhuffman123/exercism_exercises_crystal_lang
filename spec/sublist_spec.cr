e3108e16cc0678802994b127de0df3172f2bab56
daniel huffman <drhuffman123@gmail.com>
2026-06-21 22:00:30 -0400

    Sublist.. specs unpending

--- spec/sublist_spec.cr
+++ spec/sublist_spec.cr
@@ -17,23 +17,23 @@ describe "Sublist" do
       Sublist.classify([1, 2, 3] of Int32, [2, 3, 4] of Int32).should eq(Sublist::Classification::Unequal)
     end
 
-    pending "first list missing element from second list" do
+    it "first list missing element from second list" do
       Sublist.classify([1, 3] of Int32, [1, 2, 3] of Int32).should eq(Sublist::Classification::Unequal)
     end
 
-    pending "second list missing element from first list" do
+    it "second list missing element from first list" do
       Sublist.classify([1, 2, 3] of Int32, [1, 3] of Int32).should eq(Sublist::Classification::Unequal)
     end
 
-    pending "first list missing additional digits from second list" do
+    it "first list missing additional digits from second list" do
       Sublist.classify([1, 2] of Int32, [1, 22] of Int32).should eq(Sublist::Classification::Unequal)
     end
 
-    pending "order matters to a list" do
+    it "order matters to a list" do
       Sublist.classify([1, 2, 3] of Int32, [3, 2, 1] of Int32).should eq(Sublist::Classification::Unequal)
     end
 
-    pending "same digits but different numbers" do
+    it "same digits but different numbers" do
       Sublist.classify([1, 0, 1] of Int32, [10, 1] of Int32).should eq(Sublist::Classification::Unequal)
     end
   end
@@ -51,15 +51,15 @@ describe "Sublist" do
       Sublist.classify([1, 1, 2] of Int32, [0, 1, 1, 1, 2, 1, 2] of Int32).should eq(Sublist::Classification::Sublist)
     end
 
-    pending "sublist at start" do
+    it "sublist at start" do
       Sublist.classify([0, 1, 2] of Int32, [0, 1, 2, 3, 4, 5] of Int32).should eq(Sublist::Classification::Sublist)
     end
 
-    pending "sublist in middle" do
+    it "sublist in middle" do
       Sublist.classify([2, 3, 4] of Int32, [0, 1, 2, 3, 4, 5] of Int32).should eq(Sublist::Classification::Sublist)
     end
 
-    pending "sublist at end" do
+    it "sublist at end" do
       Sublist.classify([3, 4, 5] of Int32, [0, 1, 2, 3, 4, 5] of Int32).should eq(Sublist::Classification::Sublist)
     end
   end
@@ -69,15 +69,15 @@ describe "Sublist" do
       Sublist.classify([1, 2, 3] of Int32, [] of Int32).should eq(Sublist::Classification::Superlist)
     end
 
-    pending "at start of superlist" do
+    it "at start of superlist" do
       Sublist.classify([0, 1, 2, 3, 4, 5] of Int32, [0, 1, 2] of Int32).should eq(Sublist::Classification::Superlist)
     end
 
-    pending "in middle of superlist" do
+    it "in middle of superlist" do
       Sublist.classify([0, 1, 2, 3, 4, 5] of Int32, [2, 3] of Int32).should eq(Sublist::Classification::Superlist)
     end
 
-    pending "at end of superlist" do
+    it "at end of superlist" do
       Sublist.classify([0, 1, 2, 3, 4, 5] of Int32, [3, 4, 5] of Int32).should eq(Sublist::Classification::Superlist)
     end
   end
