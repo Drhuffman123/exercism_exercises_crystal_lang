60edc039911a067c3ff8cf52b62c3385ea91de10
daniel huffman <drhuffman123@gmail.com>
2026-06-21 20:08:13 -0400

    misc formatting

---
+++ src/nth_prime.cr
@@ -0,0 +1,16 @@
+module NthPrime
+  @@primes = [2]
+
+  def self.find_next_prime(number)
+    suspect = @@primes[-1] + 1
+    if suspect % @@primes[0] == 0
+      number == suspect
+    else
+    end
+  end
+
+  def self.prime(number : Number) : Number
+    @@primes = [2]
+    @@primes[number - 1] || find_next_prime(number - 1)
+  end
+end
