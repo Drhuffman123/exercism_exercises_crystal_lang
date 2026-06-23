b1ee73e8f5e0fe4b24e5c2998734cb0f19b223d6
daniel huffman <drhuffman123@gmail.com>
2026-06-23 14:28:01 -0400

    EliudsEggs src and specs unpending

--- spec/eliuds_eggs_spec.cr
+++ spec/eliuds_eggs_spec.cr
@@ -7,15 +7,15 @@ describe "EliudsEggs" do
     EliudsEggs.egg_count(0).should eq(0)
   end
 
-  pending "1 egg" do
+  it "1 egg" do
     EliudsEggs.egg_count(16).should eq(1)
   end
 
-  pending "4 eggs" do
+  it "4 eggs" do
     EliudsEggs.egg_count(89).should eq(4)
   end
 
-  pending "13 eggs" do
+  it "13 eggs" do
     EliudsEggs.egg_count(2000000000).should eq(13)
   end
 end
--- src/eliuds_eggs.cr
+++ src/eliuds_eggs.cr
@@ -1,5 +1,12 @@
 class EliudsEggs
-  def self.egg_count(number : Int32) : Int32
-    0
+  def self.egg_count(number : Int32) # : Int32
+    arr_eggs = [] of Int32
+    number.to_s(2).chars.map_with_index do |x,i|
+      egg_qty = x.to_i*(2**i)
+      if egg_qty > 0
+        arr_eggs << egg_qty
+      end
+    end
+    arr_eggs.size
   end
 end
