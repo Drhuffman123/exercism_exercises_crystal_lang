c54ed77327f11778300e15d6bd3d11107fecf73f
daniel huffman <drhuffman123@gmail.com>
2026-07-04 18:28:02 -0400

    FoodChain starter src and specs

---
+++ src/food_chain.cr
@@ -0,0 +1,8 @@
+module FoodChain
+  def self.recite(start : Int, finish : Int) : Array(String)
+    [
+      "I know an old lady who swallowed a fly.",
+      "I don't know why she swallowed the fly. Perhaps she'll die."
+    ]
+  end
+end
