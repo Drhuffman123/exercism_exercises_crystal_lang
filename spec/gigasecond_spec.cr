5e7ca622598d9becb205bce5d6cb45557f2ebaef
daniel huffman <drhuffman123@gmail.com>
2026-08-26 17:52:06 -0400

    re-add Gigasecond and un-pend the specs

--- spec/gigasecond_spec.cr
+++ spec/gigasecond_spec.cr
@@ -6,27 +6,28 @@ describe "Gigasecond" do
   #   BUT, WHY not when I copy the specs directly from the site
   #   and I try running the specs locally, they fail? UGH!!!!!!!
   #   For now, I am marking them all as pending
-  pending "Test 1" do
+  # And now that I re-checked it, it is working, so we un-pend the specs...
+  it "Test 1" do
     Gigasecond.from(Time.utc(2011, 4, 25))
       .should eq(Time.utc(2043, 1, 1, 1, 46, 40))
   end
 
-  pending "Test 2" do
+  it "Test 2" do
     Gigasecond.from(Time.utc(1977, 6, 13))
       .should eq(Time.utc(2009, 2, 19, 1, 46, 40))
   end
 
-  pending "Test 3" do
+  it "Test 3" do
     Gigasecond.from(Time.utc(1959, 7, 19))
       .should eq(Time.utc(1991, 3, 27, 1, 46, 40))
   end
 
-  pending "Test 4" do
+  it "Test 4" do
     Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
       .should eq(Time.utc(2046, 10, 2, 23, 46, 40))
   end
 
-  pending "Test 5" do
+  it "Test 5" do
     Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
       .should eq(Time.utc(2046, 10, 3, 1, 46, 39))
   end
--- src/gigasecond.cr
+++ src/gigasecond.cr
@@ -4,6 +4,7 @@ module Gigasecond
     #   BUT, WHY not when I copy the specs directly from the site
     #   and I try running the specs locally, they fail? UGH!!!!!!!
     #   For now, I am marking them all as pending
+    # And now that I re-checked it, it is working, so we un-pend the specs...
     Time.utc(start.year, start.month, start.day, start.hour, start.minute,
       start.second + 1000*start.millisecond
     ) + 1_000_000_000.seconds
